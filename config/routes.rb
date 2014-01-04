90886d1c78c321bc9670ca697ef584fc11692b2a
Felix Norton <felix@succinctdesign.co.za>
2014-01-04 18:19:53 +0200

    signup added properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pintresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "signup" => "pages#signup"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
